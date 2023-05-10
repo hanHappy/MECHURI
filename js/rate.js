986729a0e24250bfe872bae38b2c1e9189a432bb
hanHappy <sangmin950706@gmail.com>
2023-05-10 23:52:35 +0900

    검색 화면 및 모달 작성 완료
    - 검색어 입력 후 식당 리스트 페이지 작성 완료
    - 최근 검색어 페이지 작성 완료
    - 평가 페이지에서 평가 버튼 클릭 시 나타나는 모달창 작성 완료
    
    (해야 할 것)
    - 평가 결과 화면 작성

---
+++ js/rate.js
@@ -0,0 +1,34 @@
+// 가격 스크롤 ----------------------------------------
+let slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
+
+// 평가 확인 모달 --------------------------------------
+// 평가하기 버튼 클릭 시 모달 창 열기
+window.onload = function(){
+    rateBtn.onclick = openModal;
+}
+
+// 모달 열기 함수
+function openModal() {
+    document.querySelector('.modal').style.display = 'block';
+  }
+  
+  // 모달 닫기 함수
+  function closeModal() {
+    document.querySelector('.modal').style.display = 'none';
+  }
+  
+  // 닫기 버튼 클릭 시 모달 닫기
+  document.querySelector('.no').addEventListener('click', closeModal);
+  
+  // 모달 창 외부 클릭 시 모달 닫기
+  window.addEventListener('click', function(event) {
+    if (event.target.classList.contains('modal')) {
+      closeModal();
+    }
+  });
