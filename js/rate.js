f744297a88daaf47350371763a3a634483142ff6
hanHappy <sangmin950706@gmail.com>
2023-05-12 17:01:23 +0900

    평가 페이지 완료

--- js/rate.js
+++ js/rate.js
@@ -11,24 +11,24 @@ slider.oninput = function () {
 // 평가하기 버튼 클릭 시 모달 창 열기
 window.onload = function(){
     rateBtn.onclick = openModal;
-}
 
-// 모달 열기 함수
-function openModal() {
-    document.querySelector('.modal').style.display = 'block';
-  }
-  
-  // 모달 닫기 함수
-  function closeModal() {
-    document.querySelector('.modal').style.display = 'none';
-  }
-  
-  // 닫기 버튼 클릭 시 모달 닫기
-  document.querySelector('.no').addEventListener('click', closeModal);
-  
-  // 모달 창 외부 클릭 시 모달 닫기
-  window.addEventListener('click', function(event) {
-    if (event.target.classList.contains('modal')) {
-      closeModal();
+    // 모달 열기 함수
+    function openModal() {
+        document.querySelector('.modal').style.display = 'block';
+      }
+      
+    // 모달 닫기 함수
+    function closeModal() {
+      document.querySelector('.modal').style.display = 'none';
     }
-  });
+    
+    // 닫기 버튼 클릭 시 모달 닫기
+    document.querySelector('.no').addEventListener('click', closeModal);
+    
+    // 모달 창 외부 클릭 시 모달 닫기
+    window.addEventListener('click', function(event) {
+      if (event.target.classList.contains('modal')) {
+        closeModal();
+      }
+    });
+}
