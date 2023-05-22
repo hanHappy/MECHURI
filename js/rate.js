2c38c1f320faca6910a1f0d022dde000a3831fb0
hanHappy <sangmin950706@gmail.com>
2023-05-22 12:51:50 +0900

    평가 관련 페이지 작성 중
    - 검색 결과 화면 작성 완료
    - 평가 결과 페이지 작성해야 함

--- js/rate.js
+++ js/rate.js
@@ -1,35 +1,39 @@
-// 가격 스크롤 ----------------------------------------
-let slider = document.getElementById("my-range");
-let output = document.getElementById("demo");
-output.innerHTML = slider.value;
+window.addEventListener("load", function () {
+  // 슬라이더 --------------------------------------
+  let slider = document.querySelector("#my-range");
+  let output = document.querySelector("#demo");
 
-slider.oninput = function () {
-    output.innerHTML = this.value;
-}
+  // 슬라이더 조작 -> input 값 업데이트
+  slider.oninput = function () {
+    output.value = this.value;
+  }
 
-// 평가 확인 모달 --------------------------------------
-// 평가하기 버튼 클릭 시 모달 창 열기
-window.onload = function(){
-    let rateBtn = document.querySelector('#rate-btn');
-    rateBtn.onclick = openModal;
+  // 평가 확인 모달 ------------------------------------------
+  let rateBtn = document.querySelector('#rate-btn');
+  let modal = document.querySelector('.modal');
+  let no = this.document.querySelector('.no');
 
-    // 모달 열기 함수
-    function openModal() {
-        document.querySelector('.modal').style.display = 'block';
-      }
-      
-    // 모달 닫기 함수
-    function closeModal() {
-      document.querySelector('.modal').style.display = 'none';
-    }
-    
-    // 닫기 버튼 클릭 시 모달 닫기
-    document.querySelector('.no').addEventListener('click', closeModal);
-    
-    // 모달 창 외부 클릭 시 모달 닫기
-    window.addEventListener('click', function(event) {
-      if (event.target.classList.contains('modal')) {
-        closeModal();
-      }
-    });
-}
+  // 모달 열기
+  function openModal() {
+    modal.style.display = 'block';
+  }
+
+  // 모달 닫기 (아니오 클릭 시)
+  function closeModal() {
+    modal.style.display = 'none';
+  }
+
+  // 모달 닫기 (모달 외부 클릭 시)
+  window.addEventListener('click', function (e) {
+    if (e.target!=modal)
+      return;
+    closeModal();
+  })
+
+  // 평가 버튼 -> 모달 열기
+  rateBtn.onclick = openModal;
+
+  // 닫기 버튼 -> 모달 닫기
+  no.onclick = closeModal;
+
+});
